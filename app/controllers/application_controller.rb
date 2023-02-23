04343bb1426bf971474a339004c2f8942558c73f
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-02-23 13:57:26 -0700

    Add rescue from record not found to application controller, pass edge case testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def record_not_found(exception)
+    render json: ErrorSerializer.not_found(exception.message), status: :bad_request
+  end
 end
