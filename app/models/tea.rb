d8536882b9d41df0b9df48febd41f3b7471e0661
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-02-23 10:23:55 -0700

    Add relationships and validations to models, pass testing

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,7 @@
 class Tea < ApplicationRecord
+  has_many :subscriptions
+  has_many :customers, through: :subscriptions
+
+  validates_presence_of :title, :description, :temperature, :brew_time
+  validates :temperature, :brew_time, numericality: true
 end
