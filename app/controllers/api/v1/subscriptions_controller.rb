2ce707c12b2592a1e72a9e6f0aaec498196753d0
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-02-23 12:18:18 -0700

    Add before action in controller to find customer

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  before_action :find_customer
+  
   def index
     require 'pry'; binding.pry
   end
+
+  private
+
+    def find_customer
+      @customer = Customer.find(params[:customer_id])
+    end
 end
