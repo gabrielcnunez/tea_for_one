94f735cec8b75e63e1a833bee07fca754144ecf3
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-02-23 18:14:03 -0700

    Add customer_id and tea_id validations to subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,6 @@ class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
 
-  validates :title, :price, :status, :frequency, presence: true
+  validates :title, :price, :status, :frequency, :customer_id, :tea_id, presence: true
   validates_numericality_of :price
 end
